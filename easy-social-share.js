f2d0e5cb939d96099ce2319fd68a122cab7eb072
Brian Mitchell <bman4789@gmail.com>
2014-12-11 11:39:38 -0600

    Add Tumblr support

--- easy-social-share.js
+++ easy-social-share.js
@@ -5,7 +5,7 @@ angular.module('td.easySocialShare', [])
     return {
       link: function (scope, elem, attrs) {
         var i,
-            sites = ['twitter', 'facebook', 'linkedin', 'google-plus', 'reddit'],
+            sites = ['twitter', 'facebook', 'linkedin', 'google-plus', 'reddit', 'tumblr'],
             theLink,
             links = attrs.shareLinks.toLowerCase().split(','),
             pageLink = encodeURIComponent($location.absUrl()),
@@ -38,6 +38,9 @@ angular.module('td.easySocialShare', [])
               break;
             case 'reddit':
                theLink = 'http://reddit.com/submit?url=' + pageLink + '&title=' + pageTitleUri;
+              break;
+            case 'tumblr':
+               theLink = 'http://www.tumblr.com/share/link?url=' + pageLink + '&amp;name=' + pageTitleUri;
               break;
           }
 
